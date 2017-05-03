148ae49ce449d146f895e5e4a302331c09a37d94
FRANCE Frédéric <frederic.france@free.fr>
2017-05-03 11:58:41 +0200

    test

--- connection.js
+++ connection.js
@@ -1,30 +1,26 @@
 /**
  * module to connect to mysql database
  */
-var mysql = require('mysql');
- 
-function Connection() 
-{
-  this.pool = null;
- 
-  this.init = function() 
-  {
-    this.pool = mysql.createPool({
-      connectionLimit: 10,
-      host: 'localhost',
-      user: 'csharp',
-      password: 'csharp',
-      database: 'intervention'
-    });
-  };
- 
-  this.acquire = function(callback) 
-  {
-    this.pool.getConnection(function(err, connection) 
-    {
-      callback(err, connection);
-    });
-  };
+var mysql = require('mysql2');
+
+function Connection() {
+    this.pool = null;
+
+    this.init = function() {
+        this.pool = mysql.createPool({
+            connectionLimit: 10,
+            host: 'localhost',
+            user: 'csharp',
+            password: 'csharp',
+            database: 'intervention'
+        });
+    };
+
+    this.acquire = function(callback) {
+        this.pool.getConnection(function(err, connection) {
+            callback(err, connection);
+        });
+    };
 }
- 
+
 module.exports = new Connection();
